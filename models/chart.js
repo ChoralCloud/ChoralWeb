ed995159cf6ab0e0ebc4f6c69756b083fb0c4ec6
Chris Kelly <cpjkelly@gmail.com>
2017-07-13 21:27:09 -0700

    Update chart schema

--- models/chart.js
+++ models/chart.js
@@ -2,12 +2,13 @@ var mongoose = require('./setup'),
   ObjectId = mongoose.Schema.Types.ObjectId;
 
 var chartSchema = new mongoose.Schema({
-  userId: { type: ObjectId, required: '{PATH} is required!' },
-  rootChoralId: { // refers to choral_id in cassandra
-    type: String,
+  userId: {
+    type: ObjectId,
+    required: '{PATH} is required!'
+  },
+  rootChoralId: { // refers to the root choral whose value the chart will display
+    type: ObjectId,
     required: '{PATH} is required!',
-    unique: true,
-    minLength: 128
   }
 });
 
