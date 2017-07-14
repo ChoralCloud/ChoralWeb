1068950f56a2b9cea504316b8c88bcf4322f4542
Chris Kelly <cpjkelly@gmail.com>
2017-07-13 21:08:34 -0700

    Add to choral schema

--- models/choral.js
+++ models/choral.js
@@ -1,7 +1,7 @@
 var mongoose = require('./setup'),
   ObjectId = mongoose.Schema.Types.ObjectId;
-
 var uniqueValidator = require('mongoose-unique-validator');
+var srs = require('secure-random-string');
 
 var childSchema = new mongoose.Schema({
   id: { type: ObjectId, required: '{PATH} is required!' }
@@ -9,21 +9,51 @@ var childSchema = new mongoose.Schema({
 
 var choralSchema = new mongoose.Schema({
   children: [childSchema],
-  userId: { type: ObjectId, required: '{PATH} is required!' }
+  userId: { type: ObjectId, required: '{PATH} is required!' },
+  choralId: { // refers to choral_id in cassandra
+    type: String,
+    required: '{PATH} is required!',
+    unique: true,
+    minLength: 128
+  },
+  func: {
+    type: String,
+    required: [
+      function() { return this.type == 'choral' },
+      '{PATH} is required for non-device chorals!'
+    ],
+    default: "function(data) { return data[0] || 1; }"
+  },
+  sampleRate: {
+    type: Number,
+    default: 5,
+    min: 1,
+  },
+  choralType: {
+    type: String,
+    enum: ['choral', 'device'],
+    required: '{PATH} is required!'
+  }
 });
 
 // Create a new choral belonging to the user passed in the arguments
 // user should be a user model
-choralSchema.statics.createNew = function (user, cb) {
+choralSchema.statics.createNew = function (attrs, cb) {
   console.log("CREATING CHORAL");
   var newChoral = new Choral();
   newChoral.userId = attrs.user.id;
+  newChoral.func = attrs.func;
+  newChoral.sampleRate = attrs.sampleRate;
+  // if this is not unique, the save will simply fail right now
+  newChoral.choralId = srs({ length: 128 });
+  newChoral.choralType = 'choral';
   newChoral.save((err) => {
     if (err) {
       console.log(err)
       return;
     }
     console.log("CREATED CHORAL");
+    console.log(newChoral);
     cb();
   });
 };
