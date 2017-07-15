8dbe0da28d54e49354e4510d71fd577b27b75770
Andrei Taylor <andreirtaylor@gmail.com>
2017-07-14 17:56:20 -0700

    Add mock up for how we update the list of chorals

--- worker/updateChorals.js
+++ worker/updateChorals.js
@@ -1,15 +1,11 @@
 var client = require('redis').createClient(process.env.REDIS_STORE_URI)
 var Choral = require('../models/choral')
 
-client.keys('*', function (err, keys) {
-  if (err) return console.log(err)
-
-  // proof of concept that I can hit redis
-  keys.forEach((key)=> {
-    client.get('0', (err, val) => {
-      if (err) return console.log(err)
-
-      console.log(val)
+setInterval(() => {
+  console.log("Updating New Chorals")
+  Choral.getAllChorals((err, chorals) => {
+    chorals.forEach((choral) => {
+      // if the choral isnt running run it here
     })
   })
-});
+}, 0 | process.env.UPDATE_NEW_CHORALS)
