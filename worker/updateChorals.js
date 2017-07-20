829a3699c341479bbd0b36aee59347591847409c
Andrei Taylor <andreirtaylor@gmail.com>
2017-07-20 15:51:08 -0700

    Switch to Node VM for async stuff

--- worker/updateChorals.js
+++ worker/updateChorals.js
@@ -3,7 +3,7 @@ var Choral = require('../models/choral')
 var request = require('request')
 
 setInterval(() => {
-  // this doesnt run the chorals according to their timeouts it just runs 
+  // this doesnt run the chorals according to their timeouts it just runs
   // all of the chorals every UPDATE_NEW_CHORALS seconds
   Choral.getAllChorals((err, chorals) => {
     if(err) return console.log(err.message)
@@ -19,7 +19,10 @@ setInterval(() => {
         client.hgetall(child.choralId, (err, data) => {
           if(err) return console.log(err.message)
 
-          param[child.name] = data;
+          // json comes in as a string, parse it first
+          param[child.name] = {}
+          param[child.name].data = JSON.parse(data.device_data);
+          param[child.name].device_timestamp = data.device_timestamp | 0;
           // this is just so I dont have to import a promise library
           if(Object.keys(param).length == choral.children.length){
             // Amazon lambda functions would make this a viable solution to
@@ -34,17 +37,16 @@ setInterval(() => {
 }, (0 | process.env.UPDATE_NEW_CHORALS) || 5 )
 
 function runComputation(children, choral){
-  const {VM} = require('vm2');
-
+  const {NodeVM} = require('vm2');
   var config = {
     func: choral.func,
     children: children,
-    result: { },
+    callback: get_callBack(choral),
   }
 
-  const vm = new VM({
+  const vm = new NodeVM({
     require: {
-      external: true
+      external:true
     },
     sandbox: config,
     // this gives us the option to use a timeout if we see fit
@@ -56,14 +58,10 @@ function runComputation(children, choral){
   // this is call is finished.
   try{
     vm.run(`
-      const callback = function(res) {
-        result.output = res;
-      };
       eval("var f = " + func); f(children, callback) `, 'vm.js')
   } catch (e) {
     sendChoralData(choral, { error: e.message })
   }
-  sendChoralData(choral, config.result.output)
 }
 
 // uses a closure to run sendChoralData with the choral in question
