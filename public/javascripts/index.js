182e55979d96d223d08d921d06a989c12e240ca2
james <jamesjaywoo@gmail.com>
2017-07-21 14:44:50 -0700

    Add functionality to copy id's to clipboard

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,42 @@
+function addViewChoralHandlers() {
+  var $buttonNodes = document.getElementsByClassName('view-choral-btn');
+
+  for (var i = 0; i < $buttonNodes.length; i++) {
+    var $node = $buttonNodes[i];
+    $node.onclick = viewChoralHandler;
+  }
+}
+
+function viewChoralHandler(e) {
+  $.ajax({ // send a GET request
+    url: '/chorals/' + this.dataset.choralId,
+    type: 'GET'
+  })
+    .always(() => {
+      window.location.href = 'chorals/' + this.dataset.choralId;
+    });
+}
+
+function addCopyChoralHandlers() {
+  var $buttonNodes = document.getElementsByClassName('copy-choral-btn');
+
+  for (var i = 0; i < $buttonNodes.length; i++) {
+    var $node = $buttonNodes[i];
+    $node.onclick = copyChoralHandler;
+  }
+}
+
+function copyChoralHandler(e) {
+  var dummy = document.createElement("input");
+  document.body.appendChild(dummy);
+  dummy.setAttribute("id", "dummy_id");
+  document.getElementById("dummy_id").value = this.dataset.choralId;
+  dummy.select();
+  document.execCommand('copy', false, this.dataset.choralId);
+  document.body.removeChild(dummy);
+  alert("Copied to clipboard!");
+}
+
 function addDeleteChoralHandlers() {
   var $buttonNodes = document.getElementsByClassName('delete-choral-btn');
 
@@ -17,6 +56,9 @@ function deleteChoralHandler(e) {
     });
 }
 
+
 $(document).ready(function() {
   addDeleteChoralHandlers();
+  addCopyChoralHandlers();
+  addViewChoralHandlers();
 });
