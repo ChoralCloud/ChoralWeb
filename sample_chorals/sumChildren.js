2799f23e2ef6ba6c890369cb39ab660ad6341682
Chris Kelly <cpjkelly@gmail.com>
2017-07-22 19:19:28 -0700

    Add comment

--- sample_chorals/sumChildren.js
+++ sample_chorals/sumChildren.js
@@ -1,4 +1,6 @@
 function(children, done) {
+  // Sum every value of every child and return that
+  // value as the new value of this choral
   var children = Object.values(children)
   var sum = 0;
   for(var i = 0; i < children.length; i++) {
