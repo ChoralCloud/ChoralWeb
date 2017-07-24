0ae4ae930b90ffb32cb7d6ebc3995d3d1ccfc5d2
Chris Kelly <cpjkelly@gmail.com>
2017-07-24 00:58:16 -0700

    Move and update sample function

---
+++ sample_chorals/defaults/sumChildren.js
@@ -0,0 +1,18 @@
+
+function(children, done) {
+  /* Sample choral func.
+   * Sum every first-level value of every child and return that
+   * Nested values will break this
+   * Returns the sum as the new value of this choral
+   */
+  var children = Object.values(children);
+  var sum = 0;
+  for (child in children) {
+    var data = children[child].data;
+    var dataValues = Object.values(data);
+    for (value in dataValues) {
+      sum += dataValues[value];
+    }
+  }
+  done({ 'sum_of_children': sum })
+}
